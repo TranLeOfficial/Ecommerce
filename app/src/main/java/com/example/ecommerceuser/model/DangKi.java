94bdd631ef92c5339db821589602c499330d77f8
votai-77 <vothanhtai823@gmail.com>
2020-11-14 11:01:15 +0700

    ManHinh
    Login-DK-DonHang

---
+++ app/src/main/java/com/example/ecommerceuser/model/DangKi.java
@@ -0,0 +1,16 @@
+package com.example.ecommerceuser.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecommerceuser.R;
+
+public class DangKi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ki);
+    }
+}
