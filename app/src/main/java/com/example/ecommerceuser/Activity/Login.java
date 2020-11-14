73641e94c8a0f9f70d9132b5fdf2cbbaa27da941
TranLeOfficial <tranthanhnhank123@gmail.com>
2020-11-14 07:12:27 +0700

    v2

---
+++ app/src/main/java/com/example/ecommerceuser/Activity/Login.java
@@ -0,0 +1,18 @@
+package com.example.ecommerceuser.Activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ecommerceuser.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
