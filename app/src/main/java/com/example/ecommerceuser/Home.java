9b90f172fa3451c826857cb7e9693fda287c3f0e
TranLeOfficial <tranthanhnhank123@gmail.com>
2020-11-17 16:14:54 +0700

    v2

--- app/src/main/java/com/example/ecommerceuser/Home.java
+++ app/src/main/java/com/example/ecommerceuser/Home.java
@@ -1,29 +1,92 @@
 package com.example.ecommerceuser;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+
+import com.example.ecommerceuser.Activity.Login;
 import com.google.android.material.navigation.NavigationView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //Bien
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         AnhXa();
+        setSupportActionBar(toolbar);
+//        Hide or show item
+        menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_home).setVisible(false);
+        menu.findItem(R.id.nav_login).setVisible(false);
+        menu.findItem(R.id.nav_alarm).setVisible(false);
+        menu.findItem(R.id.nav_cate).setVisible(false);
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+        menu.findItem(R.id.nav_search).setVisible(false);
+
+//        --------------------------- NavigationView Drawer Menu--------------------------
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     private void AnhXa() {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolBar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_login:
+                Intent intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_cate:
+                break;
+            case R.id.nav_alarm:
+                break;
+            case R.id.nav_logout:
+                break;
+            case R.id.nav_profile:
+                break;
+            case R.id.nav_search:
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
